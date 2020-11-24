895a0fcc0c6f0df6dd5977cb302fe7b96215262e
CpObvious <getObvious@gmail.com>
2020-11-24 18:35:43 +0300

    11
    11

--- js/script.js
+++ js/script.js
@@ -1,23 +1,51 @@
+let numberElements = document.querySelector('.number-elements');
+let textContainer = document.querySelector('.text-container');
+let chkImage = document.querySelector('.chk-image');
+let rangeText = document.querySelector('.size-text');
+let sizeText = document.querySelector('.size');
+
+let newElements = document.querySelector('.new-elements');
+
 let btnCreate = document.querySelector('.btn-create');
 let btnRemove = document.querySelector('.btn-remove');
-let newElements = document.querySelector('.new-elements');
-let numberElements = document.querySelector('.number-elements');
+
 let elem;
 
-btnCreate.onclick = function() {
-    for(let i = 0; i < +numberElements.value; i++) {
+rangeText.oninput = function() {
+    let fontSize = rangeText.value;
+    let children = newElements.childNodes;
 
-        elem = document.createElement('p');
-        elem.textContent = "Новый абзац...";
-        elem.classList.add('elem');
+    sizeText.textContent = fontSize;
 
-        newElements.appendChild(elem);
+    for (let i = 0; i < children.length; i++) {
+        children[i].style.fontSize = fontSize + 'px';
     }
 }
 
-let children = newElements.childNodes;
+btnCreate.onclick = function() {
+    for(let i = 0; i < +numberElements.value; i++) {
+        elem = document.createElement('p');
+        elem.textContent = textContainer.value;
+        elem.classList.add('elem');
+        elem.style.fontSize = sizeText.textContent + 'px';
+
+        newElements.appendChild(elem);
+
+        if(chkImage.checked) {
+            let image = document.createElement('img');
+            image.src = 'img/photo.jpg';
+            image.alt = 'Изображение в абзаце';
+            image.title = 'Это картинка';
+            image.classList.add('photo');
+
+            elem.appendChild(image);
+        }
+    }
+}
 
 btnRemove.onclick = function() {
+    let children = newElements.childNodes;
+    
     for(let i = children.length - 1; i > -1; i--) {
         newElements.removeChild(children[i]);
     }
